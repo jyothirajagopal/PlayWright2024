c3fe3ef09ee15ebbd4e957e2dee7b1f8e90f243a
jyothirajagopal <133893034+jyothirajagopal@users.noreply.github.com>
2024-05-05 17:53:53 +0400

    Add files via upload
    May 4th Hw

---
+++ 01_dataTypes.js
@@ -0,0 +1,16 @@
+var  firstName ='Jyothi'
+var companyName = 'ENBD'
+ var mobNumber = 53456789
+  var isAutomation = true
+  var hasPlayWright;
+
+  console.log("Name is "+ firstName)
+  console.log("DataType of Name is "+ typeof firstName)
+  console.log("Company Name is "+ companyName)
+  console.log("DataType of company Name "+ typeof companyName)
+  console.log("Number is "+ mobNumber)
+  console.log("DataType of MobNumber "+ typeof mobNumber)
+  console.log("Is Automation is "+ isAutomation)
+  console.log("DataType of IsAutomation "+ typeof isAutomation)
+  console.log("hasPlayWright is "+ hasPlayWright)
+  console.log("DataType of hasPlaywright "+ typeof hasPlayWright)
---
+++ 01_letVarConst.js
@@ -0,0 +1,16 @@
+const browserVersion =  'chrome';
+function getBrowserVersion()
+{
+    const browserVersion = 'firefox'
+        if (browserVersion == 'chrome')
+        {
+             console.log("inside  if " +browserVersion);   
+        }
+
+        console.log("Inside function outside block "+ browserVersion);
+
+}
+
+getBrowserVersion();
+
+
